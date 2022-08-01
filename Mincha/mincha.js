396f6eef5d2fbe07d3fd41b0bfecba05db5d9bcd
Yehuda Schochet <sydschochet96@gmail.com>
2022-08-01 15:17:08 -0400

    Finishing up

--- Mincha/mincha.js
+++ Mincha/mincha.js
@@ -1,39 +1,68 @@
 (async function () {
     'use strict';
 
-    function geoFindMe() {
-        if (!navigator.geolocation) {
-            console.log("Geolocation is not supported by your browser");
-            return;
-        }
-        async function success(position) {
-            let latitude = position.coords.latitude;
-            let longitude = position.coords.longitude;
-            console.log(latitude, longitude);
-           console.log(await reverseGeocode(longitude, latitude)); 
-        }
-        function error() {
-            console.log("Unable to retrieve your location");
-        }
-        navigator.geolocation.getCurrentPosition(success, error);
+    const YOUR_TZDB_API_KEY = '6D69CAMKPGJZ';
+
+    //get device location
+    // function deviceLocation() {
+    //     if (!navigator.geolocation) {
+    //         console.log("Geolocation is not supported by your browser");
+    //         return;
+    //     }
+    //     async function success(position) {
+    //         let latitude = position.coords.latitude;
+    //         let longitude = position.coords.longitude;
+    //     }
+    //     function error() {
+    //         console.log("Unable to retrieve your location");
+    //     }
+    //     navigator.geolocation.getCurrentPosition(success, error);
+    // }
+ // deviceLocation();
+    async function onSuccess(pos) {
+        const { latitude, longitude } = pos.coords
+
+        const timezone = await getTimezone(latitude, longitude)
+        console.log(timezone);
+        const zmanim = await loadZmanim(latitude, longitude, timezone.zoneName)
+        console.log(zmanim.times);
+        const minutesBeforeShkia = 5;
+        const minutesBeforeShkiaMl = minutesBeforeShkia * 60 * 1000;
+
+        const shkia = new Date(zmanim.times.sunset);
+        const adjustedShkia = new Date(shkia.getTime() - minutesBeforeShkiaMl);
+
+        setInterval(() => {
+            const now = new Date();
+            if (now === adjustedShkia) {
+               alert('Time for Mincha');
+            }
+        }, 1000);
     }
 
-    async function reverseGeocode(latitude, longitude) {
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, error);
+
+    //take long and lat and return the timezone 
+    async function getTimezone(latitude, longitude) {
         try {
-            const response = await fetch(`http://www.mapquestapi.com/geocoding/v1/reverse?key=tlWV2N335ajVxYGwHIzrcQ6gaWB6jsPV&location=${latitude},${longitude}&includeNearestIntersection=true`);
+            const response = await fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=${YOUR_TZDB_API_KEY}&format=json&by=position&lat=${latitude}&lng=${longitude}`);
             if (!response.ok) {
                 throw new Error(`${response.status} ${response.statusText}`);
             }
             return await response.json();
 
         } catch (status) {
-            console.error('OOPS Error', error);
+            console.error('OOPS Error',status);
         }
     }
-
-    async function loadZmanim() {
+//loads the zmanim 
+    async function loadZmanim(latitude,longitude,timezone) {
         try {
-            const response = await fetch('https://www.hebcal.com/zmanim?cfg=json&zip=08701');
+            const response = await fetch(`https://www.hebcal.com/zmanim?cfg=json&latitude=${latitude}&longitude=${longitude}&tzid=${timezone}`);
             if (!response.ok) {
                 throw new Error(`${response.status} ${response.statusText}`);
             }
@@ -44,21 +73,21 @@
         }
     }
 
-    geoFindMe();
+   
     
-    const minutesBeforeShkia = 5;
-    const minutesBeforeShkiaMl = minutesBeforeShkia * 60 * 1000;
-    const zmanim = await loadZmanim();
+    // const minutesBeforeShkia = 5;
+    // const minutesBeforeShkiaMl = minutesBeforeShkia * 60 * 1000;
+    // const zmanim = await loadZmanim();
 
-    const shkia = new Date(zmanim.times.sunset);
-    const adjustedShkia = new Date(shkia.getTime() - minutesBeforeShkiaMl);
+    // const shkia = new Date(zmanim.times.sunset);
+    // const adjustedShkia = new Date(shkia.getTime() - minutesBeforeShkiaMl);
 
-    setInterval(() => {
-        const now = new Date();
-        if (now === adjustedShkia) {
-            console.log('now is bigger', now);
-        }
-    }, 1000);
+    // setInterval(() => {
+    //     const now = new Date();
+    //     if (now === adjustedShkia) {
+    //         console.log('now is bigger', now);
+    //     }
+    // }, 1000);
 
 
 })();
