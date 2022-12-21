e2afe327ed389f39cea12b7e0101a58197ecea3e
Yehuda Schochet <sydschochet96@gmail.com>
2022-12-21 16:14:42 -0500

    add coments, fix yellow spot glitch

--- wordleHelp/src/index.js
+++ wordleHelp/src/index.js
@@ -4,23 +4,27 @@ import wordlist from 'an-array-of-english-words'
 
 //get list of engish five letter words
 let words = wordlist.filter(word => word.length === 5);
+//function filterWords(letters,wordList,included) {
+//   for (let i = 0; i < letters.length; i++) {
+//     const letter = letters[i];
+//     let filteredList = wordList.filter(word => {
+//       if(included){
+//         word.includes(letter)
+//       }else{
+//          !word.includes(letter)
+//       }
 
+//     });
+//     return filteredList;
+//   }
+// }
 $('#done').on('click', () => {
   let filteredList = words;
   //filters out letters that you want excluded
   let excludedLetters = $('#excluded').val();
-  console.log(excludedLetters);
   for (let i = 0; i < excludedLetters.length; i++) {
     const letter = excludedLetters[i];
-    console.log(letter);
-   filteredList= filteredList.filter(word => !word.includes(letter))
-  }
-
-   let includedLetters = $('#included').val();
-  for (let i = 0; i < includedLetters.length; i++) {
-    const letter = includedLetters[i];
-    console.log(letter);
-    filteredList = filteredList.filter(word => word.includes(letter))
+    filteredList = filteredList.filter(word => !word.includes(letter))
   }
 
   //array of letter in the correct spots
@@ -33,31 +37,34 @@ $('#done').on('click', () => {
   ]
   for (let i = 0; i < greenLetters.length; i++) {
     const letter = greenLetters[i];
-    console.log(letter);
     if (letter !== '') {
-     filteredList= filteredList.filter(word => word[i].includes(letter));
+      filteredList = filteredList.filter(word => word[i].includes(letter));
     }
   }
-
-  let yellowLetters = [
+  
+  //array of letters in incorrect spots
+  let yellowSpots = [
     $('#firstYellow').val(),
     $('#secondYellow').val(),
     $('#thirdYellow').val(),
     $('#fourthYellow').val(),
     $('#fifthYellow').val()
   ]
-  for (let i = 0; i < yellowLetters.length; i++) {
-    const letter = yellowLetters[i];
-    console.log(letter);
-    if (letter !== '') {
-      filteredList = filteredList.filter(word => !word[i].includes(letter));
+  //filter words with letter in the wrong spot
+  for (let i = 0; i < yellowSpots.length; i++) {
+    const spot = yellowSpots[i];
+      if (spot !== '') {
+        for (let j = 0; j < spot.length; j++) {
+          filteredList = filteredList.filter(word => word.includes(spot[j]))
+        filteredList = filteredList.filter(word => !word[i].includes(spot[j]));
+      }
     }
+    
   }
-
   $('#filteredWords').text(filteredList)
 });
 
-$('#clear').on('click',()=>{
+$('#clear').on('click', () => {
   $('.inputs').val('');
   $('#filteredWords').text('');
 })
